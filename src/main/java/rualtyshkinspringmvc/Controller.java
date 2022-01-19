54c8ed841c5c9435121a0fffc15f3c2db27c1aad
altyshkin <29455komandor@mail.ru>
2022-01-19 18:34:42 +0400

    Замена xml конфигураций на Java class с той же функциональностью . Однако не работает

--- src/main/java/rualtyshkinspringmvc/Controller.java
+++ src/main/java/rualtyshkinspringmvc/Controller.java
@@ -1,12 +1,9 @@
-package ru.altyshkin.springmvc;
-
-
+package rualtyshkinspringmvc;
 import org.springframework.web.bind.annotation.GetMapping;
-
 @org.springframework.stereotype.Controller
 public class Controller {
     //указание на то какой url будет приводить к данному методу
-    @GetMapping("/Hello-world")
+    @GetMapping("/hello-world")
     public String sayHello(){
         return "hello_world";
     }

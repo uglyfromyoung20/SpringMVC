8f55c145a4965beb771398910b65a989297f8aaf
altyshkin <29455komandor@mail.ru>
2022-01-21 16:24:11 +0400

    Передача параметров в Get запрос с помощью HttpServletRequest request

--- src/main/java/rualtyshkinspringmvc/controllers/FirstController.java
+++ src/main/java/rualtyshkinspringmvc/controllers/FirstController.java
@@ -1,12 +1,18 @@
 package rualtyshkinspringmvc.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class FirstController {
    @GetMapping("/hello")
-   public  String HelloPage(){
+   public  String HelloPage(HttpServletRequest request ){
+       String name = request.getParameter("name");
+       String surname = request.getParameter("surname");
+       System.out.println("Hello"+ name + " " + surname);
        return "first/hello";
    }
 

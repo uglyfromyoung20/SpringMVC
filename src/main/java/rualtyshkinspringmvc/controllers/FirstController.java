39c295bda7a5ac590110590916dc5ddb0514d4cc
altyshkin <29455komandor@mail.ru>
2022-01-21 16:43:24 +0400

    Аннотация @RequestParam  передает параметры в наш Get запрос .Если мы не будем передавать параметры , у нас появится ошибка однако это решается с помощью required=false

--- src/main/java/rualtyshkinspringmvc/controllers/FirstController.java
+++ src/main/java/rualtyshkinspringmvc/controllers/FirstController.java
@@ -3,15 +3,18 @@ package rualtyshkinspringmvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class FirstController {
    @GetMapping("/hello")
-   public  String HelloPage(HttpServletRequest request ){
-       String name = request.getParameter("name");
-       String surname = request.getParameter("surname");
+
+   public  String HelloPage(@RequestParam (value="name", required=false) String name ,
+                            @RequestParam (value="surname",required=false) String surname){
+
        System.out.println("Hello"+ name + " " + surname);
        return "first/hello";
    }
